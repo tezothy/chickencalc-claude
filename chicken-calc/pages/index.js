f833c0e68267b8a7f09665e86731bc250ecd01a3
tezothy <contact@tezothy.com>
2025-03-07 05:20:51 +0000

    Chicken Calc First Merge

---
+++ chicken-calc/pages/index.js
@@ -0,0 +1,10 @@
+import ChickenCalculator from '../components/ChickenCalculator';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>My Chicken Calculator App</h1>
+      <ChickenCalculator />  // This line changed from McChickenCalculator
+    </div>
+  );
+}
